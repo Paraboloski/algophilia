7915004ab33d18747e84c6c864a48c59487f5a7c
AndreaPetracca <andrea.petracca@vidyasoft.it>
2026-04-25 18:03:00 +0200

    dev v1

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from typing import cast
-
 import flet as ft
 from pathlib import Path
-from app.view.fallback import Fallback
-from app.data.database import Database
 from app.core.result import Err
+from app.data.database import Database
 from app.core.exception import AppError
+from app.data.seeder import seed_database
 from app.services.registry import load_all
 from app.config import logger, Log, LoggingLevel, settings, Panic
 from app.view.components.ui.toast import ToastLevel, ToastManager
@@ -18,6 +16,10 @@ _LEVEL_MAP = {
     LoggingLevel.ERROR:   ToastLevel.ERROR,
 }
 
+class AppContext:
+    def __init__(self, page: ft.Page):
+        self.page = page
+        self.toast = ToastManager(page, 20)
 
 async def init_app() -> Database:
     load_all()
@@ -27,37 +29,24 @@ async def init_app() -> Database:
         schema=settings.SCHEMA_PATH
     )
 
-    res_conn = await db.connect()
-    if isinstance(res_conn, Err):
-        Panic._panic(f"Impossibile connettersi al database: {res_conn.error}")
+    _conn = await db.connect()
+    if isinstance(_conn, Err):
+        Panic._panic(f"Impossibile connettersi al database: {_conn.error}")
 
-    from app.data.seeder import seed_database
-    res_seed = await seed_database(db)
-    if isinstance(res_seed, Err):
-        Panic._panic(f"Inizializzazione dati (seeding) fallita: {res_seed.error}")
+    _seed = await seed_database(db)
+    if isinstance(_seed, Err):
+        Panic._panic(f"Inizializzazione dati (seeding) fallita: {_seed.error}")
 
     return db
 
-
-class PageWithToast(ft.Page):
-    toast: "ToastManager"
-
-
 def main(page: ft.Page) -> None:
-    bgIMG = str(settings.APP_PAGE_BG_FALLBACK_IMG)
-    width = int(settings.APP_PAGE_WIGHT)
-    height = int(settings.APP_PAGE_HEIGHT)
-    
-    page.window.width = width
-    page.window.height = height
-    page = cast(PageWithToast, page)
-    page.toast = ToastManager(page, 20)
-    
+    ctx = AppContext(page)
+
     def _on_log(log: Log) -> None:
         toast_level = _LEVEL_MAP.get(log.level)
         if toast_level is None:
             return
-        page.toast.show(log.message, toast_level)
+        ctx.toast.show(log.message, toast_level)
 
     logger.subscribe(_on_log)
 
@@ -66,27 +55,11 @@ def main(page: ft.Page) -> None:
             db = await init_app()
             page.session.store.set("db", db)
             logger.info("init OK")
-            Fallback(
-                page,
-                "OK",
-                "DB seeded",
-                width,
-                height,
-                bgIMG
-            )
         except AppError as e:
-            Fallback(
-                page,
-                "Errore applicativo",
-                str(e),
-                width,
-                height,
-                bgIMG
-            )
+            logger.error(f"init NOT OK: {str(e)}")
 
     page.run_task(start)
     page.on_close = lambda _: logger.shutdown()
-
 
 if __name__ == "__main__":
     ft.run(main, assets_dir="app/assets")
